b9f7ed159ae062d963a219f0803aa9df6bf78515
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2024-07-23 10:16:56 +0300

    Update FoodSystem.cs

--- Content.Server/Nutrition/EntitySystems/FoodSystem.cs
+++ Content.Server/Nutrition/EntitySystems/FoodSystem.cs
@@ -185,10 +185,10 @@ public sealed class FoodSystem : EntitySystem
             target: target,
             used: food)
         {
+            BreakOnHandChange = false,
             BreakOnMove = forceFeed,
             BreakOnDamage = true,
             MovementThreshold = 0.01f,
-            BreakOnHandChange = false,
             DistanceThreshold = MaxFeedDistance,
             // Mice and the like can eat without hands.
             // TODO maybe set this based on some CanEatWithoutHands event or component?
