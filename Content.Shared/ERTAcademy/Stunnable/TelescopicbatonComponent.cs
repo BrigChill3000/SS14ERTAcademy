246ffacb34326cb44e32cb6d36f024b7975bb368
Debiloid3000 <143325309+Debiloid3000@users.noreply.github.com>
2024-07-24 12:34:46 +0300

    ???

---
+++ Content.Shared/ERTAcademy/Stunnable/TelescopicbatonComponent.cs
@@ -0,0 +1,12 @@
+using Content.Shared.ERTAcademy.Stunnable;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Server.Stunnable.Components;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(SharedTelescopicbatonSystem))]
+public sealed partial class TelescopicbatonComponent : Component
+{
+	
+}
--- Resources/Locale/en-US/ertacademy/ertacademy/newerabitches/policebaton-component.ftl
+++
@@ -1,4 +0,0 @@
-### Telescopic baton
-
-comp-policebaton-telescopic-examined-on = Baton [color=darkgreen]on[/color].
-comp-policebaton-telescopic-examined-off = Baton [color=darkred]off[/color].
